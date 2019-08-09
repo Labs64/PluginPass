902cb38584bbe01a3c58872d4249b08d11e8a1db
Slava Rudkovskiy <xraskolx@gmail.com>
2019-08-09 14:45:26 +0300

    Issue #2 add files to aoutoload

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+// Traits
+require_once(__DIR__ . '/inc/common/traits/class-pluginpass-plugable.php');
+require_once(__DIR__ . '/inc/common/traits/class-pluginpass-validatable.php');
 
+// Activator/Deactivator
+require_once(__DIR__ . '/inc/core/class-activator.php');
+require_once(__DIR__ . '/inc/core/class-deactivator.php');
+
+// Guard
 require_once(__DIR__ . '/inc/common/class-pluginpass-guard.php');
